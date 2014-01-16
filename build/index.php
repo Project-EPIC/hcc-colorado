822093ed8a8821e9a81093ee51623ec1eca64fe2
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-15 21:59:02 -0700

    Updated build again

--- build/index.php
+++ build/index.php
@@ -27,7 +27,7 @@ http://epic.cs.colorado.edu/build/?task=publications and then all is done automa
             case 'courses':
                 echo ("Courses file...<br />");
                 echo ("Changing Directory to jekyll root and listing directory:<br />");
-                passthru("cd ~/tmp/hcc-colorado; ls; whoami; jekyll build; whoami"); 
+                passthru("cd ~/tmp/hcc-colorado; ls; rm -r _data; whoami"); 
                 
                 break;
             case 'publications':
